7c7b20121309d723d873b184074aa88ee5901cf5
Mai <comfybyte@proton.me>
2024-05-06 03:38:52 -0300

    pkgs: remove unused script

--- scripts/retag/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, writeShellApplication, ... }:
-writeShellApplication {
-  name = "retag";
-  runtimeInputs = with pkgs; [ coreutils imv gnome.zenity ];
-  text = builtins.readFile ./run.sh;
-}
