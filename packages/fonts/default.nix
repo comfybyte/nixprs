39112cb284765c5884a142c067825456ef9b9571
Mai <comfybyte@proton.me>
2024-06-15 13:30:55 -0300

    fonts: remove `zpix-nerd-font`

--- packages/fonts/default.nix
+++ packages/fonts/default.nix
@@ -1,5 +1,4 @@
 pkgs: {
   scientifica-nerd-font = import ./scientifica-nerd-font pkgs;
   cutivemono-nerd-font = import ./cutivemono-nerd-font pkgs;
-  zpix-nerd-font = import ./zpix-nerd-font pkgs;
 }
