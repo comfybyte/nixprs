d20d9be42c978e7e7d93ecf8f70add79248ed953
Mai <comfybyte@proton.me>
2024-07-09 23:27:44 -0300

    fonts: add `bedstead-nerd-font`

--- packages/fonts/default.nix
+++ packages/fonts/default.nix
@@ -2,4 +2,5 @@ pkgs: {
   scientifica-nerd-font = import ./scientifica-nerd-font pkgs;
   cutivemono-nerd-font = import ./cutivemono-nerd-font pkgs;
   spleen-nerd-font = import ./spleen-nerd-font pkgs;
+  bedstead-nerd-font = import ./bedstead-nerd-font pkgs;
 }
