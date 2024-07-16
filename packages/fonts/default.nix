f01aebdf4302fa34d872c0e92f475ccb45b10f36
Mai <comfybyte@proton.me>
2024-07-16 19:08:03 -0300

    pkgs(fonts): add `effects-eighty-nerd-font`

--- packages/fonts/default.nix
+++ packages/fonts/default.nix
@@ -3,4 +3,5 @@ pkgs: {
   cutivemono-nerd-font = import ./cutivemono-nerd-font pkgs;
   spleen-nerd-font = import ./spleen-nerd-font pkgs;
   bedstead-nerd-font = import ./bedstead-nerd-font pkgs;
+  effects-eighty-nerd-font = import ./effects-eighty-nerd-font pkgs;
 }
