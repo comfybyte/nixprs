ba76032089acc51eadabb98900e2811e455aba01
Mai <mai@ttm.sh>
2024-09-03 03:05:47 -0300

    fonts: fix package name (tehe~)

--- packages/fonts/default.nix
+++ packages/fonts/default.nix
@@ -4,5 +4,5 @@ pkgs: {
   spleen-nerd-font = import ./spleen-nerd-font pkgs;
   bedstead-nerd-font = import ./bedstead-nerd-font pkgs;
   effects-eighty-nerd-font = import ./effects-eighty-nerd-font pkgs;
-  departure-mono-eighty-nerd-font = import ./departure-mono-nerd-font pkgs;
+  departure-mono-nerd-font = import ./departure-mono-nerd-font pkgs;
 }
