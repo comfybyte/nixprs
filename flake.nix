637f624ca39e1e98f3e7547876f3fd85b229d946
Mai <mai@ttm.sh>
2024-09-03 03:03:03 -0300

    fonts: add `departure-mono-nerd-font` and a teeny devenv

--- flake.nix
+++ flake.nix
@@ -4,7 +4,14 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       eachSystem = fn:
         nixpkgs.lib.genAttrs systems (s: fn nixpkgs.legacyPackages.${s});
-    in { packages = eachSystem (pkgs: import ./packages pkgs); };
+    in { 
+      packages = eachSystem (pkgs: import ./packages pkgs); 
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ nil nixfmt ];
+        };
+        });
+    };
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
