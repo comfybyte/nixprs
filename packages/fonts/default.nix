ca6843d2df3a7e81813385ce7c232b9437665832
Mai <mai@ttm.sh>
2024-09-24 19:21:32 -0300

    fonts: add `zpix-nerd-font`

--- packages/fonts/default.nix
+++ packages/fonts/default.nix
@@ -5,4 +5,5 @@ pkgs: {
   bedstead-nerd-font = import ./bedstead-nerd-font pkgs;
   effects-eighty-nerd-font = import ./effects-eighty-nerd-font pkgs;
   departure-mono-nerd-font = import ./departure-mono-nerd-font pkgs;
+  zpix-nerd-font = import ./zpix-nerd-font pkgs;
 }
